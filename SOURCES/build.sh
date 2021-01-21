f1f356b90e43dbd488be91d47b53ef4ab2889dc7
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2021-01-20 20:00:01 -0500

    build includes VF

--- SOURCES/build.sh
+++ SOURCES/build.sh
@@ -1,11 +1,42 @@
 # source venv/bin/activate
 set -e
 
-mkdir -p ../FONTS ../FONTS/ttf ../FONTS/otf
+mkdir -p ../FONTS ../FONTS/ttf/ ../FONTS/ttf/static ../FONTS/otf #../FONTS/woff2
+
+function postprocess_vf {
+    gftools fix-nonhinting $1 $1.fix
+    mv $1.fix $1
+    gftools fix-dsig -f $1
+    gftools fix-unwanted-tables $1 -t MVAR
+}
+
+echo "GENERATING VFs"
+
+VF_FILE=../FONTS/ttf/REM\[wght]\.ttf
+fontmake -g REM-Roman.glyphs -f -o variable --output-path $VF_FILE
+
+VF_FILEit=../FONTS/ttf/REM-Italic\[wght]\.ttf
+fontmake -g REM-Italics.glyphs -f -o variable --output-path $VF_FILEit
+
+
+echo "POST PROCESSING VFs"
+
+postprocess_vf $VF_FILE
+postprocess_vf $VF_FILEit
+
+gftools gen-stat "$VF_FILE" "$VF_FILEit" --axis-order wght ital --inplace
+
+rm ../FONTS/ttf/*gasp.ttf
+
+
+#============= Generating TTFs ====================
+#==================================================
+
 
 echo "GENERATING TTFs"
-fontmake -g REM-Roman.glyphs -i -f -o ttf --output-dir ../FONTS/ttf
-fontmake -g REM-Italics.glyphs -i -f -o ttf --output-dir ../FONTS/ttf
+fontmake -g REM-Roman.glyphs -i -f -o ttf --output-dir ../FONTS/static/ttf
+fontmake -g REM-Italics.glyphs -i -f -o ttf --output-dir ../FONTS/static/ttf
+
 
 echo "POST PROCESSING TTFs"
 ttfs=$(ls ../FONTS/ttf/*.ttf)
@@ -17,10 +48,13 @@ do
 	gftools fix-dsig -f $ttf;
     
 	gftools fix-hinting $ttf;
-    mv "$ttf.fix" $ttf;
-
-    
+    mv "$ttf.fix" $ttf;    
 done
+
+
+#============= Generating OTFs ====================
+#==================================================
+
 
 echo "GENERATING OTFs"
 fontmake -g REM-Roman.glyphs -i -o otf --output-dir ../FONTS/otf
